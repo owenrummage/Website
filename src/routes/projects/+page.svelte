c732a2058d860a5c274855d235f0e56b41d46b84
Owen Rummage <owen@Owens-MacBook-Pro.local>
2025-07-06 09:46:37 -0500

    fix build

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import ContactSection from '$lib/components/ContactSection.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 
